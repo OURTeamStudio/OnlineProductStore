de5b2f449a490a9772ca8ad7ad536039a68d2c16
gB4Lt0g <gpoloten4ikg@gmail.com>
2023-12-17 22:01:20 +0200

    Add register page UI with field's validation

--- OnlineProductStore/Server/Data/DataContext.cs
+++ OnlineProductStore/Server/Data/DataContext.cs
@@ -92,6 +92,6 @@ namespace OnlineProductStore.Server.Data
 
         public DbSet<Product> Products { get; set; }
         public DbSet<Category> Categories { get; set; }
-
+        public DbSet<User> Users { get; set; }
     }
 }
